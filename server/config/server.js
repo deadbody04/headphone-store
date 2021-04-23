5444994bb74f351002026dab018177d23c5f1a3d
deadboy04 <warfrreim@mail.ru>
2021-04-23 14:40:50 +0700

    etle

--- server/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'cfa6862efe96b6a21a2a2308540dfc1e'),
-    },
-  },
-});
--- server/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'f59c5b96-cfdf-4805-89d8-be8461e038d7'
-};
