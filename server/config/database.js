36d7c7c9ce98b3bbd564bfa52ed73466aba8a728
deadboy04 <warfrreim@mail.ru>
2021-04-23 15:06:16 +0700

    dfg

--- server/config/database.js
+++ server/config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.xucnn.mongodb.net'),
+        host: env('DATABASE_HOST'),
         srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'headphone-store'),
-        username: env('DATABASE_USERNAME', 'ilya'),
-        password: env('DATABASE_PASSWORD', '720f69710'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
