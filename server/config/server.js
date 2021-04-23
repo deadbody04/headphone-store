35efe434726ade0ab48a31424b4c14c386eb63a6
deadboy04 <warfrreim@mail.ru>
2021-04-23 14:58:48 +0700

    tyru

--- server/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '808909e5faacfa8311f7fb32ff88b291'),
-    },
-  },
-});
