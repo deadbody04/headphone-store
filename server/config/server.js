c2780a045b26525971d0a81eecbc248ddc45d720
deadboy04 <warfrreim@mail.ru>
2021-04-23 15:05:25 +0700

    ghf

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bed401755d8b57cbc2ba5654eaf36318'),
+    },
+  },
+});
